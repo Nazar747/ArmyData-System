00e3c974839203798c74c06a81de7d8eb66ce9dc
Nazar Matviichuk <matvichuk231488@gmail.com>
2026-04-16 10:21:39 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'register.html'))
+})
+
+app.get('/log-in', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'log-in.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Система ArmyData запущена: http://localhost:${PORT}`)
+})
