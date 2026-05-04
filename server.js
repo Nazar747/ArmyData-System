7c631c45989d1551d23728f989f5ef74d3a07792
Nazar Matviichuk <matvichuk231488@gmail.com>
2026-05-04 19:41:12 +0300

    connect mongodb

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -22,6 +27,18 @@ app.get('/profile', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'profile.html'))
 })
 
-app.listen(PORT, () => {
-    console.log(`Система ArmyData запущена: http://localhost:${PORT}`)
+const connect_db = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Зв'язок із базою даних успішний")
+    } catch (err) {
+        console.error("Помилка підключення до бази даних: ", err.message)
+        process.exit(1)
+    }
+}
+
+connect_db().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Система ArmyData запущена: http://localhost:${PORT}`)
+    })
 })
